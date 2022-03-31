6acdde5f437471afd59ccfb68b1eb1988116254c
cashute <juaan.caash@gmail.com>
2022-03-31 00:17:47 -0300

    asd1

---
+++ PrototiposDeJuegos/Assets/MovementScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public Transform TrfObj;
+    public float movementSpeed;
+    public MovementScript asd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var asd2 = new MovementScript();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            TrfObj.position = TrfObj.position + new Vector3( movementSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
